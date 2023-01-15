a430f7bd5e4a51dfdc9a2ef011779b4d4a10b73e
YeshayahuBennun <ybennun@yahoo.com>
2023-01-15 18:36:18 +0200

    LeetCodeRemoveElement

---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int[] case1 = {3, 2, 2, 3};
+        int[] case2 = {0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(case1, 3));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        if(nums.length==0)return 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return j;
+    }
+}
